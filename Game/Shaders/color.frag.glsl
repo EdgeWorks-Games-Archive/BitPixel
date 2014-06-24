f0f7c93f1094742a54f93fd6861105dea90aa053
LaylConway <laylconway@live.com>
2014-06-24 10:38:51 +0200

    Made the pixel shader render lines because I felt like it.

--- Game/Shaders/color.frag.glsl
+++ Game/Shaders/color.frag.glsl
@@ -1,8 +1,14 @@
 ﻿#version 330
 
-uniform vec4 Color = vec4(1.0, 1.0, 1.0, 1.0);
-
 void main()
 {
-	gl_FragColor = Color;
+	float localOffset = mod(gl_FragCoord.x + gl_FragCoord.y, 10.0);
+
+	vec4 finalColor;
+	if(localOffset < 5.0)
+		finalColor = vec4(0.6, 0.6, 0.6, 1.0);
+	else
+		finalColor = vec4(0.4, 0.4, 0.4, 1.0);
+
+	gl_FragColor = finalColor;
 }
