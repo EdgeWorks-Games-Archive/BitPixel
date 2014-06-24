f483c83b2f790f3035155217c58038b999acaa88
Layl Conway <laylconway@live.com>
2014-06-24 20:42:16 +0200

    Reverted the color fragment shader and added some graphics.

--- Game/Shaders/color.frag.glsl
+++ Game/Shaders/color.frag.glsl
@@ -1,13 +1,8 @@
 ﻿#version 330
 
+uniform vec4 Color = vec4(1.0, 1.0, 1.0, 1.0);
+
 void main()
 {
-	float localOffset = mod(gl_FragCoord.x + gl_FragCoord.y, 10.0);
-
-	vec4 finalColor =
-		localOffset < 5.0 ?
-			vec4(0.6, 0.6, 0.6, 1.0) :
-			vec4(0.4, 0.4, 0.4, 1.0);
-
-	gl_FragColor = finalColor;
+	gl_FragColor = Color;
 }
