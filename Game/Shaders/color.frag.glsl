1f889ecb8a88d1859386e924720322322bb2fe42
LaylConway <laylconway@live.com>
2014-06-24 10:41:25 +0200

    Changed pixel shader to use inline if.

--- Game/Shaders/color.frag.glsl
+++ Game/Shaders/color.frag.glsl
@@ -4,11 +4,10 @@ void main()
 {
 	float localOffset = mod(gl_FragCoord.x + gl_FragCoord.y, 10.0);
 
-	vec4 finalColor;
-	if(localOffset < 5.0)
-		finalColor = vec4(0.6, 0.6, 0.6, 1.0);
-	else
-		finalColor = vec4(0.4, 0.4, 0.4, 1.0);
+	vec4 finalColor =
+		localOffset < 5.0 ?
+			vec4(0.6, 0.6, 0.6, 1.0) :
+			vec4(0.4, 0.4, 0.4, 1.0);
 
 	gl_FragColor = finalColor;
 }
